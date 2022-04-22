d912ec646fda6aa65e3a8d8cf45170639f2a2f8d
John_Go <jooqeer@yandex.ru>
2022-04-22 21:07:11 +0300

    Edited the description

--- src/public/index.php
+++ src/public/index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../backend/route.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Location Generator 2D</title>
+    <title>Location (map) Generator 2D</title>
     <link rel="icon" type="image/svg+xml" href="dist/assets/favicon.svg">
     <link rel="icon" type="image/png" href="dist/assets/favicon.png">
     <link rel="stylesheet" href="dist/css/plugins.min.css">
