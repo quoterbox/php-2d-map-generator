9b47aa02b569be8538a65d1f8299db5cc102dea9
John_Go <jooqeer@yandex.ru>
2022-05-17 21:53:31 +0300

    Fix the name of project

--- src/public/index.php
+++ src/public/index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../Backend/Route.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Location (map) Generator 2D</title>
+    <title>Map (pattern) Generator 2D</title>
     <link rel="icon" type="image/svg+xml" href="dist/assets/favicon.svg">
     <link rel="icon" type="image/png" href="dist/assets/favicon.png">
     <link rel="stylesheet" href="dist/css/plugins.min.css">
@@ -19,7 +19,7 @@ require_once __DIR__ . '/../Backend/Route.php';
     <div id="app">
         <header>
             <div class="container">
-                <h1 class="header__brand">Location Generator 2D</h1>
+                <h1 class="header__brand">Map Generator 2D</h1>
             </div>
         </header>
         <main class="container">
