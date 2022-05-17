7cc5ff980737e663a4ec653aedf17f946520e00f
John_Go <jooqeer@yandex.ru>
2022-05-17 12:40:21 +0300

    Fix public index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../backend/route.php';
+require_once __DIR__ . '/../Backend/route.php';
 ?>
 <!doctype html>
 <html lang="en">
