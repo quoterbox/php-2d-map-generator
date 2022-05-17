33cf2b8975cf45dbd99c339f51c9396c5da4fa23
John_Go <jooqeer@yandex.ru>
2022-05-17 13:06:48 +0300

    Change path to autoload.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../Backend/route.php';
+require_once '../../vendor/autoload.php';
+require_once '../Backend/route.php';
 ?>
 <!doctype html>
 <html lang="en">
