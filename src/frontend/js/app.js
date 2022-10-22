83e2e5b6acfb4a97b6f7c61dc3b1f2ece742f2cf
John Go <jooqeer@yandex.ru>
2022-10-22 09:31:46 +0300

    Lazy load for images in Assets block

--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -2,6 +2,8 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Vue from 'vue'
+import VueLazyload from 'vue-lazyload'
+Vue.use(VueLazyload)
 
 // Storage with Vuex
 import store from './store'
@@ -9,12 +11,6 @@ import store from './store'
 // BootstrapVue
 import { BootstrapVue } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
-
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// Vue.use(BootstrapVue)
-// Vue.use(IconsPlugin)
-// import { TabsPlugin } from 'bootstrap-vue'
-// Vue.use(TabsPlugin)
 
 // Components
 Vue.component('assets-component', require('./components/AssetsComponent.vue').default);
